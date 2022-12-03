d3c89eed5cfaa7ab8eaedeb56f81e851d3f28b5e
mpl <mpl@covis.de>
2022-12-03 18:38:12 +0100

    init

---
+++ src/lib/components/OrgLauncher.svelte
@@ -0,0 +1,200 @@
+<section
+  role="dialog"
+  tabindex="-1"
+  aria-modal="true"
+  aria-labelledby="header43"
+  class="slds-modal slds-fade-in-open slds-modal_large slds-app-launcher"
+>
+  <div class="slds-modal__container">
+    <button
+      class="slds-button slds-button_icon slds-modal__close slds-button_icon-inverse"
+    >
+      <svg class="slds-button__icon slds-button__icon_large" aria-hidden="true">
+        <use
+          xlink:href="../../../node_modules/@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg#close"
+        />
+      </svg>
+      <span class="slds-assistive-text">Cancel and close</span>
+    </button>
+    <div
+      class="slds-modal__header slds-grid slds-grid_align-spread slds-grid_vertical-align-center"
+    >
+      <h2 id="header43" class="slds-text-heading_medium">App launcher</h2>
+      <div class="slds-app-launcher__header-search">
+        <div class="slds-form-element">
+          <label
+            for="app-launcher-search"
+            class="slds-form-element__label slds-assistive-text"
+            >Find an app</label
+          >
+          <div
+            class="slds-form-element__control slds-input-has-icon slds-input-has-icon_left"
+          >
+            <svg class="slds-input__icon" aria-hidden="true">
+              <use
+                xlink:href="../../../node_modules/@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg#search"
+              />
+            </svg>
+            <input
+              type="search"
+              class="slds-input"
+              id="app-launcher-search"
+              placeholder="Find an app"
+            />
+          </div>
+        </div>
+      </div>
+      <button class="slds-button slds-button_neutral">App Exchange</button>
+    </div>
+    <div
+      class="slds-modal__content slds-app-launcher__content slds-p-around_medium"
+      id="modal-content-id-1"
+    >
+      <div class="slds-section slds-is-open">
+        <h3 class="slds-section__title">
+          <button
+            aria-controls="appsContent"
+            aria-expanded="true"
+            class="slds-button slds-section__title-action"
+          >
+            <svg
+              class="slds-section__title-action-icon slds-button__icon slds-button__icon_left"
+              aria-hidden="true"
+            >
+              <use
+                xlink:href="../../../node_modules/@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg#switch"
+              />
+            </svg>
+            <span class="slds-truncate" title="All Apps">All Apps</span>
+          </button>
+        </h3>
+        <div aria-hidden="false" class="slds-section__content" id="appsContent">
+          <div
+            class="slds-assistive-text"
+            id="drag-live-region"
+            aria-live="assertive"
+          />
+          <ul class="slds-grid slds-grid_pull-padded slds-wrap">
+            <li
+              class="slds-p-horizontal_small slds-size_1-of-1 slds-medium-size_1-of-3"
+            >
+              <div
+                draggable="true"
+                class="slds-app-launcher__tile slds-text-link_reset slds-is-draggable"
+              >
+                <div class="slds-app-launcher__tile-figure">
+                  <span class="slds-avatar slds-avatar_large">
+                    <abbr
+                      class="slds-avatar__initials slds-icon-custom-27"
+                      title="Sales Cloud">SC</abbr
+                    >
+                  </span>
+                  <div class="slds-m-top_xxx-small">
+                    <button
+                      class="slds-button slds-button_icon"
+                      aria-pressed="false"
+                      title="Reorder"
+                    >
+                      <svg class="slds-button__icon" aria-hidden="true">
+                        <use
+                          xlink:href="../../../node_modules/@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg#rows"
+                        />
+                      </svg>
+                      <span class="slds-assistive-text">Reorder</span>
+                    </button>
+                  </div>
+                </div>
+                <div class="slds-app-launcher__tile-body">
+                  <a href="#">Sales Cloud</a>
+                  <p>
+                    The primary internal Salesforce org. Used to run our online
+                    sales business...
+                    <button
+                      aria-describedby="help-0"
+                      class="slds-button slds-button_reset slds-text-link"
+                      >More</button
+                    >
+                  </p>
+                  <div
+                    class="slds-popover slds-popover_tooltip slds-nubbin_top-right slds-hide"
+                    role="tooltip"
+                    id="help-0"
+                    style="position:absolute;top:80px;right:30px"
+                  >
+                    <div class="slds-popover__body" />
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <div class="slds-section slds-is-open">
+        <h3 class="slds-section__title">
+          <button
+            aria-controls="itemsContent"
+            aria-expanded="true"
+            class="slds-button slds-section__title-action"
+          >
+            <svg
+              class="slds-section__title-action-icon slds-button__icon slds-button__icon_left"
+              aria-hidden="true"
+            >
+              <use
+                xlink:href="../../../node_modules/@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg#switch"
+              />
+            </svg>
+            <span class="slds-truncate" title="All Items">All Items</span>
+          </button>
+        </h3>
+        <div
+          aria-hidden="false"
+          class="slds-section__content"
+          id="itemsContent"
+        >
+          <ul class="slds-grid slds-wrap">
+            <li
+              class="slds-col_padded slds-p-vertical_xx-small slds-size_1-of-5"
+            >
+              <a href="#" class="slds-truncate" title="Accounts">Accounts</a>
+            </li>
+            <li
+              class="slds-col_padded slds-p-vertical_xx-small slds-size_1-of-5"
+            >
+              <a href="#" class="slds-truncate" title="Announcements"
+                >Announcements</a
+              >
+            </li>
+            <li
+              class="slds-col_padded slds-p-vertical_xx-small slds-size_1-of-5"
+            >
+              <a href="#" class="slds-truncate" title="Approvals">Approvals</a>
+            </li>
+            <li
+              class="slds-col_padded slds-p-vertical_xx-small slds-size_1-of-5"
+            >
+              <a href="#" class="slds-truncate" title="Campaigns">Campaigns</a>
+            </li>
+            <li
+              class="slds-col_padded slds-p-vertical_xx-small slds-size_1-of-5"
+            >
+              <a href="#" class="slds-truncate" title="Cases">Cases</a>
+            </li>
+            <li
+              class="slds-col_padded slds-p-vertical_xx-small slds-size_1-of-5"
+            >
+              <a href="#" class="slds-truncate" title="Coaching">Coaching</a>
+            </li>
+            <li
+              class="slds-col_padded slds-p-vertical_xx-small slds-size_1-of-5"
+            >
+              <a href="#" class="slds-truncate" title="Contacts">Contacts</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<div class="slds-backdrop slds-backdrop_open" role="presentation" />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+  import OrgLauncher from '../lib/components/OrgLauncher.svelte';
+
+  let showOrgLauncher = false;
+
+  const handleOrglauncherOpen = () => {
+    showOrgLauncher = true;
+  };
+</script>
+
+{#if showOrgLauncher}
+  <OrgLauncher />
+{/if}
+
+<div class="slds-context-bar">
+  <div class="slds-context-bar__primary">
+    <div
+      class="slds-context-bar__item slds-context-bar__dropdown-trigger slds-dropdown-trigger slds-dropdown-trigger_click slds-no-hover"
+    >
+      <div class="slds-context-bar__icon-action">
+        <button
+          class="slds-button slds-icon-waffle_container slds-context-bar__button"
+          title="Description of the icon when needed"
+          on:click={handleOrglauncherOpen}
+        >
+          <span class="slds-icon-waffle">
+            <span class="slds-r1" />
+            <span class="slds-r2" />
+            <span class="slds-r3" />
+            <span class="slds-r4" />
+            <span class="slds-r5" />
+            <span class="slds-r6" />
+            <span class="slds-r7" />
+            <span class="slds-r8" />
+            <span class="slds-r9" />
+          </span>
+          <span class="slds-assistive-text">Open App Launcher</span>
+        </button>
+      </div>
+      <span class="slds-context-bar__label-action slds-context-bar__app-name">
+        <span class="slds-truncate" title="App Name">SFDX GUI</span>
+      </span>
+    </div>
+  </div>
+  <nav class="slds-context-bar__secondary" role="navigation">
+    <ul class="slds-grid">
+      <li class="slds-context-bar__item slds-is-active">
+        <a href="#" class="slds-context-bar__label-action" title="Home">
+          <span class="slds-assistive-text">Current Page:</span>
+          <span class="slds-truncate" title="Home">Home</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</div>
